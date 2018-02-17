13fe698801353621a3b3ed5520d24e830941b827
kontok <kontok93@gmail.com>
2018-02-17 12:41:01 +0300

    Done

--- task_generator_password.py
+++ task_generator_password.py
@@ -2,17 +2,12 @@ from string import digits, ascii_letters
 import random
 
 def password_generator(n):
-    password = ''
-    x =''
-    valid_values = digits + ascii_letters
-    while n !=0:
-        x = random.choice(valid_values)
-        password+=x
-        n-=1
-    yield password
-    
+    valid_values = list(digits + ascii_letters)
+    while True:
+        password = ''
+        while len(password) != n:
+            password+=random.choice(valid_values)
+        yield password
 
 
 
-
-        
